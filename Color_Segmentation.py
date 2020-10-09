d7f15d9fd2ede75f147047898a6100d297fbf6b2
Diego <di-diego@javeriana.edu.co>
2020-10-08 19:15:11 -0500

    comentado

--- Color_Segmentation.py
+++ Color_Segmentation.py
@@ -1,3 +1,4 @@
+#Se importan las librerias
 import numpy as np
 import os
 import cv2
@@ -6,7 +7,7 @@ from sklearn.mixture import GaussianMixture as GMM
 from sklearn.cluster import KMeans
 from sklearn.utils import shuffle
 
-def recreate_image(centers, labels, rows, cols):
+def recreate_image(centers, labels, rows, cols): #funcion que permite recrear la imagen segun el numero de clusters
     d = centers.shape[1]
     image_clusters = np.zeros((rows, cols, d))
     label_idx = 0
@@ -15,7 +16,6 @@ def recreate_image(centers, labels, rows, cols):
             image_clusters[i][j] = centers[labels[label_idx]]
             label_idx += 1
 
-    # Display all results, alongside original image
     plt.figure(1)
     plt.clf()
     plt.axis('off')
@@ -24,44 +24,50 @@ def recreate_image(centers, labels, rows, cols):
 
     plt.show()
 
-def clustering(image,metodo):
+def clustering(image,metodo): #funcion para crear modelos segun el metodo a utilizar
+
+    # labels guarda las predicciones de color para cada pixel de la imagen
+    # centers guarda los valores de los centros obtenidos
     if metodo == 'kmeans':
-        model = KMeans(n_clusters=n_colors, random_state=0).fit(image_array_sample)
+        model = KMeans(n_clusters=n_colors, random_state=0).fit(image_array_sample) #se crea un modelo KMEANS segun n_colors
         labels = model.predict(image_array)
         centers = model.cluster_centers_
 
     if metodo == 'gmm':
-        model = GMM(n_components=n_colors).fit(image_array_sample)
+        model = GMM(n_components=n_colors).fit(image_array_sample) #se crea un modelo GMM segun n_colors
         labels = model.predict(image_array)
         centers = model.means_
 
-    return labels,centers
+    return labels,centers #se retornan los labels y los centros
 
-def calc_distance():
-    partial_sum = 0
+def calc_distance(): #funcion para calcular la suma de distancias intra-clusters
+    partial_sum = 0 #se inicia suma parcial en 0
     for index in range(0, image_array.shape[0]):
+        # se suman las normas de las diferencias entre un pixel y su respectivo centro
         partial_sum += np.linalg.norm((image_array[index] - centers[labels[index]]), axis=0)
 
-    sums.append(partial_sum)
+    sums.append(partial_sum) #se guarda esta suma en sums
     return  sums
 
 if __name__ == '__main__':
 
-    path = r'C:\Users\di-di\OneDrive\Escritorio\imagenes_vision'#input('ingrese path: ')
-    image_name = r'bandera.png'#input('image name: ')
+    #r'C:\Users\di-di\OneDrive\Escritorio\imagenes_vision'
+    path = input('ingrese path: ') #se pide al usuario que ingrese el path donde se encuentra la imagen
+    image_name = input('image name: ') #r'bandera.png' #se pide al usuario que ingrese el nombre de la imagen
     path_file = os.path.join(path, image_name)
-    image = cv2.imread(path_file, 1)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.imread(path_file, 1) #se lee la imagen segun la ruta
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #se cambia el espacio de color (en realidad solo el orden)
 
-    metodo = input('ingrese metodo: ')
+    metodo = input('ingrese metodo: ') #se le pide al usuario que ingrese el metodo (k-means o GMM)
 
-    image = np.array(image, dtype=np.float64) / 255
-    rows, cols, ch = image.shape
-    assert ch == 3
-    image_array = np.reshape(image, (rows * cols, ch))
+    image = np.array(image, dtype=np.float64) / 255 #se convierte a flotante y se normaliza la imagen
+    rows, cols, ch = image.shape #de la imagen se obtienen las columnas, filas y canales
+    assert ch == 3 #se asegura que sea una imagen a color
+    image_array = np.reshape(image, (rows * cols, ch)) #se convierte la imagen a un arreglo unidimensional, mas cada componente tendra tres valores (RGB)
     print("Fitting model on a small sub-sample of the data..")
-    image_array_sample = shuffle(image_array, random_state=0)[:10000]
+    image_array_sample = shuffle(image_array, random_state=0)[:10000] #se escogen 10000 muestras del arreglo
 
+    #se muestra la imagen que se quiere segmentar
     plt.figure(1)
     plt.clf()
     plt.axis('off')
@@ -69,17 +75,15 @@ if __name__ == '__main__':
     plt.imshow(image)
     plt.show()
 
-    sums = []
+    sums = [] #variable que nos permitira guardar las sumas de distancias intra-clusters para cada valro de n_colors
 
-    for n_colors in range(1,11):
+    for n_colors in range(1,11): #ciclo para iterar el numero de clusters
 
-        labels,centers = clustering(image, metodo)
-        # Get labels for all points
+        labels,centers = clustering(image, metodo) #se obtienen los labels y centros segun el metodo
+        sums = calc_distance() #se calcula la suma de distancias intra-clusters
+        recreate_image(centers, labels, rows, cols) #se muestra la imagen final segmentada
 
-        sums = calc_distance()
-
-        recreate_image(centers, labels, rows, cols)
-
+    #se grafica la suma de distancias intra-cluster vs numero de clusters/gaussianas
     plt.plot(list(range(1,11)),sums)
     plt.title('Sum of intra-cluster distances vs Number of colors')
     plt.xlabel('Number of colors')
--- Geometric_Transformations.py
+++ Geometric_Transformations.py
@@ -1,33 +1,37 @@
+#Se importan las librerias
 import numpy as np
 import os
 import cv2
 
-def click_event(event, x, y, flags, params):
-    global number_points
+def click_event(event, x, y, flags, params): #funcion para atender el evento del mouse
+    global number_points #variable que nos permite contar cuantos puntos se han seleccionado
     if number_points < 3:
         if event == cv2.EVENT_RBUTTONDOWN:
-            mouse_coordinates.append([x, y])
+            mouse_coordinates.append([x, y]) #se guardan en la variable mouse_coordinates el punto seleccionado
             number_points += 1
-    else:
+    else: #si se seleccionaron 3 puntos se cierra la ventana
         number_points = 0
         cv2.destroyAllWindows()
 
-def get_points():
-    for i in range(0,len(images)):
-        cv2.imshow(windowsNames[i],images[i])
-        cv2.setMouseCallback(windowsNames[i], click_event)
+def get_points(): #funcion para guardar los puntos
+    for i in range(0,len(images)): #ciclo para guardar 6 puntos, 3 por cada imagen
+        cv2.imshow(windowsNames[i],images[i]) #se muestra la imagen
+        cv2.setMouseCallback(windowsNames[i], click_event) #se llama a la funcion para seleccionar puntos
         cv2.waitKey(0)
 
-    pts1 = np.float32(mouse_coordinates[0:3]) #lena
-    pts2 = np.float32(mouse_coordinates[3:6]) #lena_warped
+    pts1 = np.float32(mouse_coordinates[0:3]) #3 puntos seleccionados para la imagen lena
+    pts2 = np.float32(mouse_coordinates[3:6]) #3 puntos seleccionados para la imagen lena_warped
 
     return pts1,pts2
 
-def estimate_similarity(pts1,M_affine):
+def estimate_similarity(pts1,M_affine): #funcion para estimar la transformacion de similitud (metodo explicado en el documento)
 
+    #se transforman los puntos de la primera imagen a coordenadas homogeneas
     point1_homogeneous = np.append(pts1[0], np.array([1]), axis=0)
     point2_homogeneous = np.append(pts1[1], np.array([1]), axis=0)
     point3_homogeneous = np.append(pts1[2], np.array([1]), axis=0)
+
+    #se realiza la transformacion de estos puntos a partir de la transformacion afin
     new_point1 = M_affine.dot(point1_homogeneous)
     new_point2 = M_affine.dot(point2_homogeneous)
     new_point3 = M_affine.dot(point3_homogeneous)
@@ -47,77 +51,93 @@ def estimate_similarity(pts1,M_affine):
     x3_prime = new_point3[0]
     y3_prime = new_point3[1]
 
+    #vector solucion con los puntos transformados
     b = np.array([x1_prime,y1_prime,x2_prime,y2_prime,x3_prime,y3_prime])
 
+    #se crea la matriz correspondiente con los 3 puntos
     matrix1 = np.array([[x1,y1,1,0,0,0],[0,0,0,x1,y1,1],[x2,y2,1,0,0,0],[0,0,0,x2,y2,1],[x3,y3,1,0,0,0],[0,0,0,x3,y3,1]])
-    matrix2 = matrix1.T
+    matrix2 = matrix1.T #se traspone la matriz
 
-    A_1 = np.linalg.inv(matrix2.dot(matrix1))
-    A_2 = A_1.dot(matrix2)
-    A_3 = np.dot(A_2,b.T)
+    A_1 = np.linalg.inv(matrix2.dot(matrix1)) #se calcula la inversa de ese producto
+    A_2 = A_1.dot(matrix2) #se multiplica la matriz anterior por la matriz traspuesta
+    A_3 = np.dot(A_2,b.T) #por ultimo se multiplica por el vector solucion
 
+    #parametros de la matriz de similitud
     a = A_3[0]
     b = A_3[1]
+    tx = A_3[2]
     d = A_3[3]
     e = A_3[4]
+    ty = A_3[5]
 
+    #computacion de los parametros theta, (s1,s2)
     theta = np.arctan(-b/e)
     s1 = a/np.cos(theta)
     s2 = e/np.cos(theta)
-    tx = A_3[2]
-    ty = A_3[5]
 
-    print('aproximacion parametros de matriz de similitud')
+    print('approximation of similarity matrix parameters')
     print(f'theta1 = {theta}')
     print(f's1 = {s1}')
     print(f's2 = {s2}')
     print(f'tx = {tx}')
     print(f'ty = {ty}')
 
+    #matriz de similitud obtenida
     M_similarity = np.array([[a, b, tx],
                         [d, e, ty]])
 
     return M_similarity
 
-def calc_error(pts1,M_similarity,pts2):
+def calc_error(pts1,M_similarity,pts2): #funcion para calcular el error
+
+    # se le agregar una fila de unos (coordenadas homogeneas) a la traspuesta de los primeros puntos
+    # esto con el fin de poder multiplicar por la matriz de similitud obtenida
     pts = np.append(pts1.transpose(), np.array([[1, 1, 1]]), axis=0)
     pts_transform = M_similarity.dot(pts)
+
+    # se vuelve a trasponer la matriz para obtener los puntos originales ahora transformados
     pts_transform = pts_transform.transpose()
 
+    # se calcula la norma de la diferencia de cada punto (arreglo de 3 con el error en cada caso)
     error = np.linalg.norm(pts_transform - pts2, axis=1)
     print('Error calculated = ',error)
 
 if __name__ == '__main__':
-    mouse_coordinates = list()
-    number_points = 0
+    mouse_coordinates = list() #lista donde se guardan las coordenadas escogidas con el mouse
+    number_points = 0 #variable que nos permite contar cuantos puntos se han seleccionado, se inicializa en cero
 
-    path1 = r'C:\Users\di-di\OneDrive\Escritorio\imagenes_vision'#input('ingrese path: ')
-    image_name1 = r'lena.png'#input('image name: ')
+    #r'C:\Users\di-di\OneDrive\Escritorio\imagenes_vision'
+    path1 = input('ingrese path: ') #se pide al usuario que ingrese el path donde se encuentran las imagenes
+    image_name1 = input('image name: ') #r'lena.png' se pide al usuario que ingrese el nombre de la imagen
     path_file1 = os.path.join(path1, image_name1)
-    lena = cv2.imread(path_file1, 1)
+    lena = cv2.imread(path_file1, 1) #se lee la imagen segun la ruta
 
-    image_name2 = r'lena_warped.png'#input('image name: ')
+    image_name2 = input('image name: ') #r'lena_warped.png' se pide al usuario que ingrese el nombre de la imagen
     path_file2 = os.path.join(path1, image_name2)
     lena_warped = cv2.imread(path_file2, 1)
 
-    windowsNames = ['lena','lena_warped']
-    images = [lena,lena_warped]
+    windowsNames = ['lena','lena_warped'] #lista con los nombres de las imagenes
+    images = [lena,lena_warped] #lista con las dos imagenes seleccionadas
 
-    pts1,pts2 = get_points()
+    pts1,pts2 = get_points() #se obtienen los 6 puntos de las dos imagenes
 
-    M_affine = cv2.getAffineTransform(pts1, pts2)
+    M_affine = cv2.getAffineTransform(pts1, pts2) #se guarda en M_affine la matriz de transformacion afin
 
     print('Affine transformation: ')
     print(M_affine)
 
+    #se realiza la transformacion a la imagen lena
     image_affine = cv2.warpAffine(lena, M_affine, lena.shape[:2])
     cv2.imshow("Affine", image_affine)
 
+    #se realiza la estimacion de la transformacion de similitud y
+    M_similarity = estimate_similarity(pts1,M_affine) #matriz de similitud
 
-    M_similarity = estimate_similarity(pts1,M_affine)
+    #se realiza la tranformacion de similitud a la imagen lena
     image_similarity = cv2.warpAffine(lena, M_similarity, lena.shape[:2])
     cv2.imshow('similarity', image_similarity)
 
+    #se calcula el error
     calc_error(pts1, M_similarity, pts2)
 
     cv2.waitKey(0)
